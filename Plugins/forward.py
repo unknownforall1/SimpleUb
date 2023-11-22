d7523942b27b46cc11746eb8e4e0c04175e35a5a
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-22 21:57:26 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -2,27 +2,25 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+
 import logging
 logger = logging.getLogger(__name__)
 
 import asyncio
-from pyrogram import filters, enums
-from pyrogram.errors import FloodWait
+from pyrogram import filters
 from bot import channelforward
 from config import Config 
-xdchannel = -1002031514094
-@channelforward.on_message(filters.channel | filters.private | filters.chat)
+
+@channelforward.on_message(filters.channel)
 async def forward(client, message):
     # Forwarding the messages to the channel
    try:
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
-         if xdchannel == int(to_channel):
+         if int(to_channel) == int(to_channel):
             func = message.copy if Config.AS_COPY else message.forward
             await func(int(to_channel), Config.AS_COPY)
-            await asyncio.sleep(10)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
-            await asyncio.sleep(5)
+            await asyncio.sleep(1)
    except Exception as e:
       logger.exception(e)
-
