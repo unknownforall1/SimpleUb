8533b4c322708255e609a64389606f923e1a9502
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-22 21:45:52 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -17,14 +17,12 @@ async def forward(client, message):
    try:
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
-         if 1 == int(from_channel):
+         if xdchannel == int(to_channel):
             func = message.copy if Config.AS_COPY else message.forward
             await func(int(to_channel), Config.AS_COPY)
             await asyncio.sleep(10)
-   except FloodWait as e:
-            await asyncio.sleep(e.x)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
-            await asyncio.sleep(10)
+            await asyncio.sleep(5)
    except Exception as e:
       logger.exception(e)
 
