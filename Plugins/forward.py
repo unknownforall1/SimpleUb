de4a0b3749ea08f26f3bcf2050c2cb130171b044
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-22 21:25:15 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -6,7 +6,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 import asyncio
-from pyrogram import filters
+from pyrogram import filters, enums
+from pyrogram.errors import FloodWait
 from bot import channelforward
 from config import Config 
 xdchannel = -1002031514094
@@ -16,18 +17,14 @@ async def forward(client, message):
    try:
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
-         if message.chat.id == int(from_channel):
+         if 1 == int(from_channel):
             func = message.copy if Config.AS_COPY else message.forward
             await func(int(to_channel), Config.AS_COPY)
+            await asyncio.sleep(10)
+   except FloodWait as e:
+            await asyncio.sleep(e.x)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
-            await asyncio.sleep(1)
+            await asyncio.sleep(10)
    except Exception as e:
       logger.exception(e)
 
-
-@channelforward.on_message((filters.private | filters.channel | filters.chat) & (filters.document | filters.video | filters.audio | filters.photo) , group=4)
-async def forward(client, message):
-    func = message.copy if Config.AS_COPY else message.forward
-    await func(int(xdchannel), Config.AS_COPY)
-    logger.info("A MSG FORWARDED", xdchannel)
-    await asyncio.sleep(1)
