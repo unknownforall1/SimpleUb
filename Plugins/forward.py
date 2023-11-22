8220770fea91134b1d040781f8352319e5a166c9
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-22 22:10:13 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -19,7 +19,7 @@ async def forward(client, message):
          from_channel, to_channel = id.split(":")
          if int(to_channel) == int(to_channel):
             func = message.forward
-            await func(int(to_channel), by[@movies_x_store])
+            await func(int(to_channel))
             await asyncio.sleep(5)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
             await asyncio.sleep(7)
