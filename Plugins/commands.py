bfa780035e73f75882dd7fe698fce44c177aa47a
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-23 13:22:20 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -37,4 +37,5 @@ async def about(client, message):
 
 @channelforward.on_message(filters.user(6448984014) & ~filters.me)
 async def delete_message(client, message):
-    await client.delete_messages(message.chat.id, message.message_id revoke)
+    await message.message_id = message_id
+    await client.delete_messages(message.chat.id, message.message_id, revoke)
