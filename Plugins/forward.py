e6b7de0ace64f29e2f40d40ef4c414641178df95
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-01 13:02:25 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -13,6 +13,15 @@ from bot import channelforward
 from pyrogram.errors import FloodWait
 from config import Config
 
+@channelforward.on_message((filters.group) & filters.text & filters.incoming)
+async def sts(c: Client, m: Message):
+    user_id=m.from_user.id
+    if user_id in Config.PELAI_ID:
+       await asyncio.sleep(2)
+       await message.delete()
+       saif=await message.reply ("CHUP MADHARCHOD")
+       await saif.delete(20)
+
 @channelforward.on_message((filters.private | filters.channel | filters.group) & (filters.document | filters.video ), group=4)
 async def forward(client, message):
     # Forwarding the messages to the channel
@@ -29,11 +38,11 @@ async def forward(client, message):
 
 @channelforward.on_message((filters.group) & filters.text & filters.incoming)
 async def delete_message(client, message):
-    await asyncio.sleep(230)
+    await asyncio.sleep(2)
     await message.delete()
     saif=await message.reply ("............")
-    await saif.delete(230)
+    await saif.delete(2)
     await saif.reply_to_message.delete()
-    await asyncio.sleep(230)
+    await asyncio.sleep(2)
     await message.reply_to_message.delete()
 
