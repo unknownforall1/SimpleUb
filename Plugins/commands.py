77b95da3314bf00c9462b4a6f21fe7be20812495
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-03 19:06:44 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -5,10 +5,15 @@
 import logging
 logger = logging.getLogger(__name__)
 
-from pyrogram import Client, filters
+
+from pyrogram import filters, Client, enums
 from bot import channelforward
 from config import Config
 from translation import Translation
+import asyncio
+import time
+from pyrogram.errors import FloodWait
+
 
 
 ################################################################################################################################################################################################################################################
@@ -37,4 +42,18 @@ async def about(client, message):
 
 
 
+@channelforward.on_message((filters.channel) & (filters.document | filters.video ), group=4)
+async def forward(client, message):
+    # Forwarding the messages to the channel
+   try:
+      for id in Config.CHANNEL:
+         from_channel, to_channel = id.split(":")
+         if message.chat.id == int(from_channel):
+            await message.chat_id
+            func = message.copy
+            await asyncio.sleep(2)
+            await func(to_channel)
+            logger.info("Forwarded a message from", from_channel, "to", to_channel)
+   except Exception as e:
+       logger.exception(e)
     
